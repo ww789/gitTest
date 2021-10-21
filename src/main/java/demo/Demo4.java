cb8a2f2b813f20f5f6561053585d1fc3750b8bbf
my123 <mytest@163.com>
2021-10-22 06:02:14 +0800

    test 2 commit

--- src/main/java/demo/Demo4.java
+++ src/main/java/demo/Demo4.java
@@ -3,5 +3,8 @@ package demo;
 public class Demo4 {
     public static void main(String[] args) {
         System.out.println("hello Demo4");
+        System.out.println("hello Demo4");
+        System.out.println("hello Demo4");
+        System.out.println("hello Demo4");
     }
 }
