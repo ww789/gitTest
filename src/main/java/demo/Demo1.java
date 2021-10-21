561136a906e5df5148825b4ac9d2269efc1f3d9b
my123 <mytest@163.com>
2021-10-22 05:21:00 +0800

    first commit

--- src/main/java/demo/Demo1.java
+++ src/main/java/demo/Demo1.java
@@ -3,5 +3,7 @@ package demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("abc");
+        System.out.println("123");
     }
 }
--- src/main/java/demo/Demo2.java
+++ src/main/java/demo/Demo2.java
@@ -1,4 +1,7 @@
 package demo;
 
 public class Demo2 {
+    public void test(){
+        System.out.println("Demo2 test");
+    }
 }
