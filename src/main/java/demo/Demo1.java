f89ff19e814b485a81f64cd50d94ffe9476414da
ww789 <92952478+ww789@users.noreply.github.com>
2021-10-22 06:20:05 +0800

    Update Demo1.java

--- src/main/java/demo/Demo1.java
+++ src/main/java/demo/Demo1.java
@@ -2,7 +2,7 @@ package demo;
 
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello~~~~~~~~~~~~~~~~~");
         System.out.println("abc");
         System.out.println("123");
         System.out.println("hello");
